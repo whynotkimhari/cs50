01583dc86aa231b0cb329f905809573a13beb263
whynotkimhari <htlulem185@gmail.com>
2023-08-29 21:41:46 +0700

    update finale

--- CS50W/projects/commerce/auctions/admin.py
+++ CS50W/projects/commerce/auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import User, Listing, Comment, Bid, UserWatchList
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Comment)
+admin.site.register(Bid)
+admin.site.register(UserWatchList)
